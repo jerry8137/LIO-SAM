90413662baa179b891304d88871834ed0822ff00
Jerry <jerry8137@gmail.com>
2022-10-18 19:08:17 +0800

    add folder

--- enter_docker.sh
+++ enter_docker.sh
@@ -5,7 +5,8 @@ docker run --init -it --rm \
     -v /etc/timezone:/etc/timezone:ro \
     --volume=/tmp/.X11-unix:/tmp/.X11-unix \
     -v ~/Documents/Jerry/docker_ws:/root/catkin_ws \
-    -v /mnt/nfs/Pacifica5/2022-04-14-p5-68/:/data:ro \
+    -v /mnt/nfs/Pacifica5/2022-04-14-p5-68/:/data/itri:ro \
+    -v ~/Documents/Jerry/liosam_sample:/data/sample:ro \
     --env="DISPLAY=$DISPLAY" \
     --device=/dev/dri \
     --env="QT_X11_NO_MITSHM=1" \

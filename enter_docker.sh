59d1600f39e3b81422f6f8e7d6ee033ef2cedad1
Jerry <jerry8137@gmail.com>
2022-10-18 16:47:26 +0800

    enter sh

---
+++ enter_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run --init -it --rm \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /etc/timezone:/etc/timezone:ro \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ~/Documents/Jerry/docker_ws:/root/catkin_ws \
+    -v /mnt/nfs/Pacifica5/2022-04-14-p5-68/:/data:ro \
+    -e DISPLAY=$DISPLAY \
+    --name liosam \
+    liosam-kinetic-xenial \
+    bash
